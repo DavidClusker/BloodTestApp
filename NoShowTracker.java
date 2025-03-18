5aa6b51a9d7ce4c57eeb41325f0e5e060e215fab
DavidClusker <116078882+DavidClusker@users.noreply.github.com>
2025-03-18 20:13:26 +0000

    Add files via upload
    This is my third adt its a dll im using to keep trak of 5 no showes

---
+++ NoShowTracker.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package patient;
+
+/**
+ *
+ * @author David
+ */
+public class NoShowTracker {
+     private class Node {
+        Patient patient;
+        Node prev, next;
+
+        public Node(Patient patient) {
+            this.patient = patient;
+        }
+    }
+
+    private Node head, tail;
+    private int size;
+
+    public NoShowTracker() {
+        head = tail = null;
+        size = 0;
+    }
+
+    public void addNoShow(Patient patient) {
+        Node newNode = new Node(patient);
+        if (size == 5) {
+            // Remove the oldest node
+            head = head.next;
+            if (head != null) {
+                head.prev = null;
+            }
+            size--;
+        }
+
+        if (tail == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public String getNoShows() {
+        StringBuilder result = new StringBuilder();
+        Node current = head;
+        while (current != null) {
+            result.append(current.patient.toString()).append("\n");
+            current = current.next;
+        }
+        return result.toString();
+    }
+    
+}
